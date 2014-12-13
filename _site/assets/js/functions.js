87ca64f5a79463c2a0e81f42a030af24416d61d9
Denis Vyazmin <strongconjunctions@gmail.com>
2014-12-13 01:08:21 +0000

    slider functionality added (still have problems with the transition)

--- _site/assets/js/functions.js
+++ _site/assets/js/functions.js
@@ -1,7 +1,31 @@
-$( document ).ready(function() {
-
-  //
-
+$(function() {
+	smoothScroll(300);
+	projectsSlider(); // when the page loads the 'projectsSlider' function will run
 });
 
 
+function smoothScroll (duration) {
+	$('a[href^="#"]').on('click', function(event) {
+
+		var target = $( $(this).attr('href') );
+
+		if( target.length ) {
+			event.preventDefault();
+			$('html, body').animate({
+				scrollTop: target.offset().top
+			}, duration);
+		}
+	});
+}
+
+
+function projectsSlider() {
+
+	$('.thumb').click(function() {
+		$('.projects-slider').css('left','-100%');
+	});
+
+	$('.return').click(function() {
+		$('.projects-slider').css('left','0%');
+	});
+}
