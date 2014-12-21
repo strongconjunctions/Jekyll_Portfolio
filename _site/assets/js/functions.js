fbccce5780c95bb28119a2adf0db19088a4dd782
Denis Vyazmin <strongconjunctions@gmail.com>
2014-12-21 04:19:47 +0000

    finished the projects section

--- _site/assets/js/functions.js
+++ _site/assets/js/functions.js
@@ -1,10 +1,39 @@
+var imgCount = 1;
+var total = 8;
 
+function slide(x) {
+	var Image = document.getElementById('img');
+	imgCount = imgCount + x;
+	if(imgCount > total){ imgCount = 1; }
+	if(imgCount < 1){ imgCount = total; }
+	Image.src = "../assets/img/projects/proj" + imgCount + ".jpg";
+}
+
+
+
+
+
+
+
+/*  $(function() {
+    $(".rslides").responsiveSlides({
+     timeout: 4000,
+     fx:   'scrollHorz',
+     next: '#next',
+     prev: '#prev',
+ 	});
+  });*/
+
+
+
+/*
 
 // This is the Projects slider function
 
 $(function() {
 	smoothScroll(300);
 	projectsSlider(); // when the page loads the 'projectsSlider' function will run
+	projectsLoad();
 });
 
 
@@ -32,7 +61,25 @@ function projectsSlider() {
 
 	$('.return').click(function() {
 		$('.projects-slider').css('left','0%');
-		$('.projects-container').hide();
+		$('.projects-container').hide(800);
 	});
 }
+
+
+function projectsLoad() {
+
+	$.ajaxSetup({ cache: true });
+
+	$('.thumb-unit').click(function() {
+
+		var $this = $(this),
+			newTitle = $this.find('strong').text(),
+			spinner = '<div class="loader">Loading...</div>',
+			newHTML = '../portf/portf1.html';
+		$('.portf').html(spinner).load(newHTML);
+		$('.p-title').text(newTitle);
+	});
+
+}*/
+
 
