98b1f19e90f2111ce4a5b2b2885b2473a2218022
strongconjunctions <strongconjunctions@gmail.com>
2015-01-24 15:26:59 +0000

    added the header resizing jquery from fittextjs.com, added a new bg, changed the color of the footer text, placed the bg's into a separate folder

--- _site/assets/js/functions.js
+++ _site/assets/js/functions.js
@@ -1,3 +1,42 @@
+
+
+// HEADER RESIZING (fittextjs.com)
+
+$(function() {
+	$(".header-class h1").fitText(1, { minFontSize: '10px', maxFontSize: '70px' });
+});
+
+
+(function( $ ){
+	$.fn.fitText = function( kompressor, options ) {
+
+		// Setup options
+		var compressor = kompressor || 1,
+			settings = $.extend({
+			'minFontSize' : Number.NEGATIVE_INFINITY,
+			'maxFontSize' : Number.POSITIVE_INFINITY
+			}, options);
+			return this.each(function(){
+
+			// Store the object
+			var $this = $(this);
+
+			// Resizer() resizes items based on the object width divided by the compressor * 10
+			var resizer = function () {
+			$this.css('font-size', Math.max(Math.min($this.width() / (compressor*10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)));
+			};
+
+			// Call once to set.
+			resizer();
+
+			// Call on resize. Opera debounces their resize by default.
+			$(window).on('resize.fittext orientationchange.fittext', resizer);
+		});
+	};
+})( jQuery );
+
+
+
 // PROJECTS SECTION CODE
 
 var imgCount = 1;
